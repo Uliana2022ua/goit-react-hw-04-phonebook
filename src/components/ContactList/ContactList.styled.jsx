d8e10a16b9dd519be574350b0a2af4670cff2c00
Uliana <ulianaastra1@gmail.com>
2023-06-10 01:28:42 +0300

    update phonebook

---
+++ src/components/ContactList/ContactList.styled.jsx
@@ -0,0 +1,11 @@
+import Styled from '@emotion/styled';
+
+export const List = Styled.ul`
+display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`;
+
